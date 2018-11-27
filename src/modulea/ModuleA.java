a275cb781933d7e19a4fecd61681622565c9caba
Student <Student@PC-33>
2018-11-27 09:23:37 +0800

    commit in netbeans for [#161402928] - update event

--- src/modulea/ModuleA.java
+++ src/modulea/ModuleA.java
@@ -18,7 +18,7 @@ public class ModuleA {
         // TODO code application logic here
         System.out.println("Hello World!");
         System.out.println("bye");
-        //System.out.println("damn");
+        System.out.println("damn");
     }
     
 }
